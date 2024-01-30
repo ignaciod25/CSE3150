fe7f1023d1543ad10a5a1b5de3e960d20c713d28
Ignacio Deleon <144495918+ignaciod25@users.noreply.github.com>
2024-01-29 23:03:41 -0500

    unit_tests_prefix_sum.cpp

--- Lab 1/unit_tests_prefix_sum.cpp
+++ Lab 1/unit_tests_prefix_sum.cpp
@@ -12,19 +12,29 @@ int nums_0[] = {};
 int nums_1[] = {2, -3, 3, 2};
 int nums_2[] = {1, 1, -1, -1};
 int nums_3[] = {-1, -1, 1, 1};
+int nums_4[] = {1, 2, 3, 4, 5};
+int nums_5[] = {-1, -2, -3, -4, -5};
 
 TEST_CASE("Testing prefix_sum function") {
     CHECK(prefix_sum(nums_0, sizeof(nums_0) / sizeof(nums_0[0])) == 0);
     CHECK(prefix_sum(nums_1, sizeof(nums_1) / sizeof(nums_1[0])) == 4);
     CHECK(prefix_sum(nums_1, sizeof(nums_1) / sizeof(nums_1[0]) - 2) == -1);
+    CHECK(prefix_sum(nums_2, sizeof(nums_2) / sizeof(nums_2[0])) == 0);
+    CHECK(prefix_sum(nums_2, sizeof(nums_2) / sizeof(nums_2[0]) - 1) == 1);
+    CHECK(prefix_sum(nums_3, sizeof(nums_3) / sizeof(nums_3[0]) - 1) == -1);
+    CHECK(prefix_sum(nums_3, sizeof(nums_3) / sizeof(nums_3[0])) == 0);
+    CHECK(prefix_sum(nums_4, sizeof(nums_4) / sizeof(nums_4[0])) == 15);
+    CHECK(prefix_sum(nums_5, sizeof(nums_5) / sizeof(nums_5[0])) == -15);
 
     SUBCASE("non-negative sum") {
         CHECK(!non_negative_sum(nums_3, sizeof(nums_3) / sizeof(nums_3[0])));
         CHECK(non_negative_sum(nums_2, sizeof(nums_2) / sizeof(nums_2[0])));
+        CHECK(non_negative_sum(nums_4, sizeof(nums_4) / sizeof(nums_4[0])));
     }
 
     SUBCASE("non-positive sum") {
         CHECK(non_positive_sum(nums_3, sizeof(nums_3) / sizeof(nums_3[0])));
         CHECK(!non_positive_sum(nums_2, sizeof(nums_2) / sizeof(nums_2[0])));
+        CHECK(!non_positive_sum(nums_4, sizeof(nums_4) / sizeof(nums_4[0])));
     }
 }
